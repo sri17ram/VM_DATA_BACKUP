94edd679e26b62225ceba7555ee5acfbb9de3d98
Sriram Ramanujam <sriramcspmi@gmail.com>
2018-07-23 05:29:58 +0530

    2018_07_23_05_29_58_1532303998

--- VM_CONFIGS/git_push.sh
+++ VM_CONFIGS/git_push.sh
@@ -4,8 +4,18 @@ date=`date +%Y_%m_%d_%H_%M_%S_%s`
 
 echo "$date"
 
+cd /home/tools/VM_DATA_BACKUP
+
 git config --global user.email "sriramcspmi@gmail.com"
+
 git config --global user.name "Sriram Ramanujam"
+
 git add *
+
 git commit -m "$date"
+
 git push
+
+date=`date +%Y_%m_%d_%H_%M_%S_%s`
+
+echo "$date"
