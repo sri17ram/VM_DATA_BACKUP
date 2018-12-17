7ca634b3d426c122ccc3b6590430fb81d1d3d149
Sriram Ramanujam <sriramcspmi@gmail.com>
2018-12-17 16:00:01 +0530

    2018_12_17_16_00_01_1545042601

--- NSE/NSEpy/dataframes.py
+++ NSE/NSEpy/dataframes.py
@@ -1,2 +1,8 @@
 import os
 os.system("python testPandas.py")
+
+from nsepy import get_history
+from datetime import date
+
+data = get_history(symbol="SUZLON", start=date(2018,1,1), end=date(2018,12,1))
+print (data.columns)
